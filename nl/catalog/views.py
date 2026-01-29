88909f95ceda57190cff38d0ffbe60f6c091f3ea
N <n_email@example.com>
2026-01-29 10:25:17 +0700

    отображение html шаблонов на главной странице

--- nl/catalog/views.py
+++ nl/catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Book, Author, Genre
 
-# Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books=Book.objects.all().count()
+    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_authors':num_authors},)

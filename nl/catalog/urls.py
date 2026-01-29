b7bffa9b67a6499a2b81223351c5197878960fe2
N <n_email@example.com>
2026-01-29 20:09:22 +0700

    сделала faq

--- nl/catalog/urls.py
+++ nl/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView #отображения шаблонов без доп логики
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('news/', views.NewsListView.as_view(), name='news'),
     path('news/<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
+    path('faq/', TemplateView.as_view(template_name='catalog/faq.html'), name='faq'),
 ]
